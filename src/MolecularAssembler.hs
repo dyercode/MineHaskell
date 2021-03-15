29747e58cf12d448efeeec1e24fff58afa932048
Jon <dyercode@users.noreply.github.com>
2021-03-15 18:39:05 -0400

    make separate type for size-validated dimensions and change functions requiring them to..require them...
    left dimensions in place because I want to keep addition and subtraction
    but don't want to wrap them in validation.
    
    I'm not super happy that I can't use the same record syntax, though I
    suspect there's a way to do so (without shadowing?).

--- src/MolecularAssembler.hs
+++ src/MolecularAssembler.hs
@@ -25,23 +25,26 @@ data Blocks = Blocks
   }
   deriving (Show)
 
+data LegalAssembler = LegalAssembler Int Int Int
+  deriving (Show, Eq)
+
 checkSmallness :: Int -> String -> Validation [String] Int
 checkSmallness d name
   | d < 3 = Failure [name ++ " is too small"]
   | otherwise = Success d
 
 -- | Tries to 'Dimensions' enforcing a minimum size of 3x3x3
-createLegalAssembler :: Int -> Int -> Int -> Validation [String] Dimensions
+createLegalAssembler :: Int -> Int -> Int -> Validation [String] LegalAssembler
 createLegalAssembler l w h =
-  Dimensions <$> checkSmallness l "length" <*> checkSmallness w "width" <*> checkSmallness h "height"
+  LegalAssembler <$> checkSmallness l "length" <*> checkSmallness w "width" <*> checkSmallness h "height"
 
 -- | alias to createDimensions supplying equal sides
-createCube :: Int -> Validation [String] Dimensions
+createCube :: Int -> Validation [String] LegalAssembler
 createCube s = first (const ["Sides were < 3"]) (createLegalAssembler s s s)
 
 -- | Calculates number of each block type needed to increase 'assembler' to 'desired' size
 -- Including refunded blocks when downgrading or changing shape
-calcBlocksNeeded :: Dimensions -> Dimensions -> Blocks
+calcBlocksNeeded :: LegalAssembler -> LegalAssembler -> Blocks
 calcBlocksNeeded assembler desired =
   calcBlocksIn desired -# calcBlocksIn assembler
 
@@ -53,20 +56,20 @@ calcBlocksNeeded assembler desired =
 (Blocks wall1 vent1 core1) -# (Blocks wall2 vent2 core2) =
   Blocks (wall1 - wall2) (vent1 - vent2) (core1 - core2)
 
-calcWallSize :: Dimensions -> Int
-calcWallSize (Dimensions l w h) = (4 * (l + w + h)) - 16
+calcWallSize :: LegalAssembler -> Int
+calcWallSize (LegalAssembler l w h) = (4 * (l + w + h)) - 16
 
 -- | Calculates the area of rectangle less a 1-sized perimiter
 lessPlanePerimiter :: Int -> Int -> Int
 lessPlanePerimiter a b = (a - 2) * (b - 2)
 
-calcVentSize :: Dimensions -> Int
-calcVentSize (Dimensions l w h) =
+calcVentSize :: LegalAssembler -> Int
+calcVentSize (LegalAssembler l w h) =
   2 * (lessPlanePerimiter l w + lessPlanePerimiter w h + lessPlanePerimiter l h)
 
-calcCoreSize :: Dimensions -> Int
-calcCoreSize (Dimensions l w h) = (l - 2) * (w - 2) * (h - 2)
+calcCoreSize :: LegalAssembler -> Int
+calcCoreSize (LegalAssembler l w h) = (l - 2) * (w - 2) * (h - 2)
 
-calcBlocksIn :: Dimensions -> Blocks
-calcBlocksIn d =
-  Blocks {wall = calcWallSize d, vent = calcVentSize d, core = calcCoreSize d}
+calcBlocksIn :: LegalAssembler -> Blocks
+calcBlocksIn la =
+  Blocks {wall = calcWallSize la, vent = calcVentSize la, core = calcCoreSize la}
