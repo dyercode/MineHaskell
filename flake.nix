dfb16c6e8ca26b63df48bc215a42da706048d0e6
Jon <dyercode@users.noreply.github.com>
2023-02-06 02:09:33 -0500

    switch to flake.

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Achievement sersr";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.cabal2nix
+            pkgs.nix-prefetch-git
+            pkgs.cabal-install
+            pkgs.ghc
+          ];
+          shellHook = ''
+            # cabal update # as predicted, a bit agressive.
+          '';
+        };
+      });
+}
