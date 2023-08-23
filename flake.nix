60f9cdc21ad535b9f91c868bcebe4a8b76c9520f
Jon <dyercode@users.noreply.github.com>
2023-08-23 06:14:31 -0400

    update flake to use dev.

--- flake.nix
+++ flake.nix
@@ -4,14 +4,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    dev.url = "github:/dyercode/dev";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, dev }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = [
+            dev.defaultPackage.${system}
             pkgs.cabal2nix
             pkgs.nix-prefetch-git
             pkgs.cabal-install
